1556188e3fc7743a1b1467578d31949367d3035a
l3v3nshtain <mikhvepriail.com>
2025-12-01 17:57:54 +0300

    debug

--- tickets-backend/src/users/users.module.ts
+++ tickets-backend/src/users/users.module.ts
@@ -3,14 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from '../schemas/user.schema';
+import { AuthModule } from '../auth/auth.module';  // <-- ВАЖНО!
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule, // <-- Добавлено, чтобы JwtService стал доступен
   ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
-
